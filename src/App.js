b7ec434fd6318a638a4c9226fb6673dbac73a7aa
Vu Doan <vudoan3112@gmail.com>
2024-04-17 10:09:47 -0500

    refactor routes to app router

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import React from 'react';
 import './App.scss';
 import { Navigation } from './components/navigation/Navigation';
 import { Footer } from './components/footer/Footer';
-import { TermsOfUse } from './pages/TermsOfUse/TermsOfUse';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage/HomePage';
-import { PrivacyPolicy } from './pages/PrivacyPolicy/PrivacyPolicy';
-
+import { AppRouter } from './router/AppRouter';
 function App() {
 	return (
 		<>
 			<Navigation />
-			<Router>
-				<Routes>
-					<Route path="/" element={<HomePage />}></Route>
-					<Route path="/terms-of-use" element={<TermsOfUse />}></Route>
-					<Route path="/privacy" element={<PrivacyPolicy />}></Route>
-				</Routes>
-			</Router>
+			<AppRouter />
 			<Footer />
 		</>
 	);
