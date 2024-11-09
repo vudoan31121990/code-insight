c2fbb176c70f9937473dd69176fcfd65935a90b3
Vu Doan <vudoan3112@gmail.com>
2024-11-09 11:22:16 -0600

    by pass the build app job

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,52 @@
-// webpack.config.js
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.scss$/, // Target .scss files
-          use: [
-            'style-loader', // Injects styles into the DOM
-            'css-loader',   // Translates CSS into CommonJS
-            {
-              loader: 'postcss-loader', // Process CSS with PostCSS plugins
-              options: {
-                postcssOptions: {
-                  plugins: [
-                    require('autoprefixer'),
-                    require('postcss-calc')() // Enable calc plugin here
-                  ]
-                }
-              }
-            },
-            'sass-loader' // Compiles Sass to CSS (Make sure this runs first)
-          ]
-        },
-      ]
-    },
-  };
-  
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.scss$/, // Target .scss files
+				use: [
+					MiniCssExtractPlugin.loader, // Extracts CSS to separate files
+					'css-loader', // Translates CSS into CommonJS
+					{
+						loader: 'postcss-loader', // Processes CSS with PostCSS plugins
+						options: {
+							postcssOptions: {
+								plugins: [
+									require('autoprefixer'),
+									require('postcss-calc')({
+										warnWhenCannotResolve: true // Warns instead of errors on calc issues
+									})
+								]
+							}
+						}
+					},
+					'sass-loader' // Compiles SCSS to CSS
+				]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({ filename: 'styles.css' }) // Extracts CSS to file
+	],
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'build'),
+		compress: true,
+		port: 4200,
+    open: true
+	}
+};
